6ba1e77c0624dd3028ad44125bb8cff8565a1483
DmytroKolisnyk2 <d.s.kolisnyk@i.ua>
2022-10-09 13:04:29 +0300

    fixed url parse

--- utils/axios.ts
+++ utils/axios.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_ROUTE;
+axios.defaults.baseURL = new URL(process.env.NEXT_PUBLIC_API_ROUTE).href;
 
 export { axios };
